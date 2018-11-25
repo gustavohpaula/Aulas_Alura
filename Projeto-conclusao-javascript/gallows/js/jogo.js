bb16a762ec390134218eb458fcb6f6e56e21f0ef
gustavohpaula <gustavohp4@gmail.com>
2018-11-25 20:37:10 -0200

    atualizando projeto

---
+++ Projeto-conclusao-javascript/gallows/js/jogo.js
@@ -0,0 +1,72 @@
+var criaJogo = function(sprite){
+
+    var palavraSecreta ='';
+    var lacunas = [];
+    var etapa = 1;
+
+    var ganhou = function () {
+        return lacunas.length
+        ? !lacunas.some(function(lacuna){
+            return lacuna == '';
+        })
+        :false;
+    };
+
+    var perdeu = function () {
+       return sprite.isFinished();
+    };
+
+    var ganhouOuPerdeu = function () {
+        return ganhou() || perdeu();
+    };
+
+    var reinicia = function () {
+        etapa = 1;
+        lacunas = [];
+        palavraSecreta = '';
+        sprite.reset();
+    };
+
+    var processaChute = function(chute){
+        var exp = new RegExp(chute, 'gi'),
+        resultado,
+        acertou = false;
+
+        while(resultado = exp.exec(palavraSecreta)){
+            acertou = lacunas[resultado.index] = chute;
+        };
+
+        if(!acertou){
+            sprite.nexteFrame();
+        };
+    };
+
+    var criaLacunas = function(){
+        lacunas = Array(palavraSecreta.length).fill('');
+    };
+    var proximaEtapa = function(){
+        etapa = 2;
+    };
+    var setPalavraSecreta = function(palavra){
+        palavraSecreta = palavra;
+        criaLacunas();
+        proximaEtapa();
+
+    };
+    var getLacunas = function(){
+        return lacunas;
+    };
+    var getEtapa = function(){
+        return etapa;
+    };
+    return {
+        setPalavraSecreta: setPalavraSecreta,
+        getLacunas: getLacunas,
+        getEtapa: getEtapa,
+        processaChute: processaChute,
+        ganhou: ganhou,
+        perdeu: perdeu,
+        ganhouOuPerdeu: ganhouOuPerdeu, 
+        reinicia: reinicia
+    };
+}
